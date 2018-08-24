adfbcbf184ed5601c7c10cbafbd74dd47c9e493f
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-08-24 14:46:37 +1000

    email

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,5 +1,6 @@
 $(function() {
   $("#contactForm .input__button").click(function() {
+    
     var data = {
       name: $("#form_name").val(),
       email: $("#form_email").val(),
@@ -11,7 +12,10 @@ $(function() {
           url: "email.php",
           data: data,
           success: function(){
-            $('.input__button').addClass('sucess');
+            $('.input__button').addClass('success');
+            $("#form_name").val() = '';
+            $("#form_email").val() = '';
+            $("#msg_text").val() = '';
           },
           error: function() {
             $('.input__button').addClass('error');
