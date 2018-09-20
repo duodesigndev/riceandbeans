c64aeb12a4527e62b3c9a5a7ef0d87c44f0d94d7
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-09-21 07:51:28 +1000

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,18 @@
-export default ((x) => {
-    init = () => {
+export default (() => {
+    var init = () => {
         initEvents();
     },
     initEvents = () => {
-        x.classList.toggle("animate")
+        $('#mobileMenu').bind({
+            click: () => {
+                console.log('Click')
+            }
+        })
+    },
+    toggleMenu = (x) => {
+        x.classList.toggle('animate')
         document.getElementsByTagName('body')[0].classList.toggle('perspective')
-        document.getElementsByClassName('main-container')[0].classList.toggle("animate-main")
+        document.getElementsByClassName('main-container')[0].classList.toggle('animate-main')
         document.getElementsByClassName('menu-mobile')[0].classList.toggle('animate-menu-mobile')
     };
     return { init: init };
